e41b1270c7c6941f899a3f9c51d27cb02f619905
omkar9405 <omkarp1101@rediffmail.com>
2020-08-01 23:41:31 +0530

    Updates

---
+++ precache-manifest.1cc4312cd9da00a13f98facec6d2f706.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c36696b868c91a86559ea0e8bf0ecd43",
+    "url": "/omkar9405/react-designs/index.html"
+  },
+  {
+    "revision": "82469002769041402cd7",
+    "url": "/omkar9405/react-designs/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "9725aa35c0cb642e6a8d",
+    "url": "/omkar9405/react-designs/static/css/main.0ee0bc56.chunk.css"
+  },
+  {
+    "revision": "82469002769041402cd7",
+    "url": "/omkar9405/react-designs/static/js/2.a52756ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/omkar9405/react-designs/static/js/2.a52756ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9725aa35c0cb642e6a8d",
+    "url": "/omkar9405/react-designs/static/js/main.89a8b54b.chunk.js"
+  },
+  {
+    "revision": "4dba41e76224ca883b34",
+    "url": "/omkar9405/react-designs/static/js/runtime-main.0c201eef.js"
+  },
+  {
+    "revision": "cba154e7488165fb665ce412ef7748f5",
+    "url": "/omkar9405/react-designs/static/media/login.cba154e7.svg"
+  }
+]);
